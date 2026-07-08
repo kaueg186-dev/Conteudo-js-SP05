0f013171c8c63d1761f1299ed3e428a3af294a2f
unknown <kaueg186@gmail.com>
2026-07-07 23:45:48 -0300

    Conteúdo aula 02

---
+++ Aula02/script.js
@@ -0,0 +1,12 @@
+//Exibindo conteúdo com innerHTML
+document.getElementById("ex1").innerHTML = "<h2>Somente a dor coletiva gera união!</h2>";
+
+//Exibindo conteúdo com o innerText
+document.getElementById("ex2").innerHTML = "Hello world";
+
+//Exibindo conteúdo com o document.write()
+document.write(50 + 50);
+
+//Saída de dados com o alert()
+//alert("Somente a dor gera união")
+console.log("Somente a dor coletiva gera união")
---
+++ Aula02/script02.js
@@ -0,0 +1,13 @@
+let x, y, z;
+x = 10;
+y = 4;
+z = x + y;
+
+document.getElementById("ex1").innerHTML = z;
+
+//Exemplo de blocos de instrução
+
+function dialogo() {
+    document.getElementById("ex2-1").innerHTML = "Olá SP05!";
+    document.getElementById("ex2-2").innerHTML = "Como estão?";
+}
