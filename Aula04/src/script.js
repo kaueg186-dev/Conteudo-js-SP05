9ffccde124afa50eeaf3f2758d6503fbf1ddb458
unknown <kaueg186@gmail.com>
2026-07-15 22:17:06 -0300

    Conteúdo Aula 04 Javascript

---
+++ Aula04/src/script.js
@@ -0,0 +1,63 @@
+//Operador de atribuição
+
+let x = 10;
+
+console.log("A variável x recebe o valor de: " + x);
+
+//Operador de Adição
+let num1 = 3;
+let num2 = 6;
+let soma = num1 + num2;
+
+console.log("A soma de 3 com 6 é: " + soma);
+
+//Operador de multiplicação
+console.log("")
+
+let mult = 5 + 5;
+
+console.log("O resultado da multiplicação de 5 por 5 é: " + mult);
+
+//Operadores aritméticos 
+console.log("");
+
+let a = 2;
+let calculo = (200 + 50) + a;
+
+console.log("O resultado da expressão é: " + calculo);
+
+//concatenando strings
+console.log("");
+
+let primeiroNome = "Kauê";
+let ultimoNome = "Oliveira";
+let nomeCompleto = primeiroNome + " " + ultimoNome;
+
+console.log("O meu nome é: " + nomeCompleto);
+console.log("")
+
+
+let Texto1 = "que dia";
+
+Texto1 += " Kauê";
+
+console.log(Texto1);
+
+console.log("");
+
+let somaNumeros = 10 + 8;
+let juntarStringNumero = "10" + 10;
+let juntarStringNumero2 = "SPO" + 5;
+
+console.log(somaNumeros);
+console.log(juntarStringNumero);
+console.log(juntarStringNumero2);
+
+//Operador de comparação
+console.log("")
+
+let n = 3;
+
+console.log("2 é maior que 3? " + (2 > n));
+console.log("7 é maior do que 3? " + (7 > n));
+
---
+++ Aula04/src/script03.js
@@ -0,0 +1,48 @@
+//Exemplo atribuição
+let a = 5;
+let exAtribuicao1 = a;
+
+console.log("Atribuindo o 5 para a: " + exAtribuicao1);
+
+//Atribuição de adição
+console.log("");
+
+let b = 20;
+
+b += 4; //Mesma coisa que: b = b + 4; 
+
+console.log(b);
+
+//Atribuição de subtração
+console.log("");
+
+let c = 10;
+c-= 2; //Mesma coisa que: c = c - 2;
+
+console.log(c);
+
+//Atribuição de multiplicação
+console.log("");
+
+
+let d = 4;
+d *= 4; //Mesma coisa que: d = d * 4;
+
+console.log(d);
+
+//Atribuição de exponenciação
+console.log("");
+
+let e = 10;
+
+e **= 6; //Mesma coisa que: e = e ** 6;
+
+console.log(e)
+
+//Atribuição de divisão
+console.log("");
+
+let f = 10;
+f /= 2; //Mesma coisa que f = f / 2;
+
+console.log(f)
