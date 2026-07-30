d6b671486fd4af3d66c436ff9eb1f3618524a255
unknown <kaueg186@gmail.com>
2026-07-30 19:09:47 -0300

    Aula 11 Javascript

---
+++ Aula11/src/script.js
@@ -0,0 +1,24 @@
+//Exemplo de método indexOf()
+let texto = "Por favor Localize a palavra 'Localize' na string";
+
+let LocalizeIndexOF = texto.indexOf("Localize");
+
+console.log(LocalizeIndexOF);
+
+console.log("");
+
+//Exemplo do método LastIndexOF()
+let texto2 = "Por favor Localize a palavra 'Localize' na string";
+
+let LocalizeLastIndexOF = texto2.lastIndexOf("Localize");
+
+console.log("");
+
+//Exemplo do método search()
+let texto3 = "Somente a dor coletiva gera união";
+
+let texto3Search = texto3.search("União");
+
+console.log(texto3Search);
+
+console.log("");
