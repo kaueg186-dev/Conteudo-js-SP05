7770dbf523f6e12a7cc68147b4178c39273a44cb
unknown <kaueg186@gmail.com>
2026-07-30 22:32:57 -0300

    Aula 12 Javascript

---
+++ Aula12/src/script.js
@@ -0,0 +1,44 @@
+//Exemplo método de objeto number.inInteger()
+let num1 = 12;
+
+console.log(Number.isInteger(num1));
+
+console.log("");
+
+//Exemplo método de objeto number.isFinite()
+let num2 = 789;
+
+console.log(Number.isFinite(num2));
+
+console.log("");
+
+//Exemplo método de objeto number.isNam()
+let num3 = 2;
+
+console.log(Number.isNaN(num3));
+console.log(Number.isNaN(100 / "Kauê"));
+
+console.log("");
+
+//Exemplo método de objeto number.isSafeInteger()
+let num4 = 78;
+
+console.log(Number.isSafeInteger(num4));
+console.log(Number.isSafeInteger(123123123123123123123123));
+
+console.log("");
+
+//Exemplo método de objeto number.parseFloat()
+let num5 = "5234";
+
+console.log(Number.parseFloat(num5));
+
+console.log("");
+
+//Exemplo método de objeto number.parseInt()
+let num6 = "1356";
+
+console.log(Number.parseInt(num6));
+
+console.log("");
+
---
+++ Aula12/src/script03.js
@@ -0,0 +1,28 @@
+//Introdução a função
+
+function primeirafuncao() {
+    return "Hello world";
+}
+
+let ativaFuncao = primeirafuncao();
+
+console.log(ativaFuncao);
+
+function adicao(a, b) {
+    return a + b;
+}
+
+let soma1 = adicao(5, 6);
+let concatenacao1 = adicao("Olá, ", "SP05! ");
+
+console.log(soma1);
+console.log(concatenacao1);
+
+console.log("");
+
+function nomeCompleto(primeiroNome, ultimoNome) {
+    return primeiroNome + " " + ultimoNome;
+}
+    console.log(nomeCompleto("Kauê", "Oliveira"));
+
+    
